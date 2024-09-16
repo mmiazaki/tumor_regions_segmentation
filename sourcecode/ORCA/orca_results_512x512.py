f52fd5ec918572246ea60d0d5c056527da94dd4d
Mauro Miazaki <maurom@gmail.com>
2024-09-16 20:44:06 -0300

    Results

--- sourcecode/ORCA/orca_results_512x512.py
+++ sourcecode/ORCA/orca_results_512x512.py
@@ -1,19 +1,25 @@
 from sourcecode.results_utils import *
 
 if __name__ == '__main__':
-    dir = '../../results/'
-    filename = [
-        '001-ORCA512-BCELoss',
-        '002-ORCA512-BCELoss',
-        '003-ORCA512-L1Loss',
-        '004-ORCA512-MSELoss',
-        '005-ORCA512-PoissonNLLLoss',
-        '006-ORCA512-HuberLoss',
-        '007-ORCA512-SmoothL1Loss',
-        '008-ORCA512-L1Loss',
-        '009-ORCA512-HingeEmbeddingLoss',
-        '010-ORCA512-SoftMarginLoss']
+    dir = '../../results/_r001-ORCA512/'
+    csv_input = 'orca_training_accuracy_loss_all.csv'
+    csv_output_epoch = 'epoch_measures.csv'
+    csv_output_final = 'final_measures.csv'
 
-    for fname in filename:
-        print('Ploting: '+fname)
-        plot_graph(dir+fname+'.csv', dir+fname+'.png', False, False, 'FCN Training - RCAug - '+fname[4:])
+    dataset = [
+        '012-ORCA512-BCELoss-Full',
+        '013-ORCA512-L1Loss-Full',
+        '014-ORCA512-MSELoss-Full',
+        '015-ORCA512-HuberLoss-Full',
+        '016-ORCA512-SmoothL1Loss-Full',
+        '020-ORCA512-BCELoss-random8',
+        '022-ORCA512-BCELoss',
+        '023-ORCA512-BCELoss-random8',
+        '024-ORCA512-BCELoss-random9',
+        '025-ORCA512-BCELoss-random8',
+        '026-ORCA512-L1Loss-random9']
+
+    for ds in dataset:
+        print('::: Ploting/calculating measures: '+ds+' :::')
+        plot_graph(dir+ds+'/'+csv_input, dir+ds+'.png', 400, False, False, 'FCN - RCAug - '+ds)
+        calculate_results(dir + ds + '/' + csv_input, dir + ds + '/' + csv_output_epoch, dir + csv_output_final, ds)
